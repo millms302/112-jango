644e97e4d6192297adc8d6a9730c7173edf9efa7
Mike Miller <millms302@gmail.com>
2026-01-08 22:20:11 -0600

    assign 1

--- pages/urls.py
+++ pages/urls.py
@@ -5,6 +5,6 @@ from .views import AboutPageView
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("home/", HomePageView.as_view(), name="homeTwo"),
-    path("", AboutPageView.as_view(), name="about")
+    path("", AboutPageView.as_view(), name="about"),
     path("about/", AboutPageView.as_view(), name="aboutTwo")
 ]
